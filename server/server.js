d575eb10092f70c3e186265135ded39a44333b56
DarkyWarky <292mhatre@gmail.com>
2024-10-01 11:56:14 +0530

    final push

--- server/server.js
+++ server/server.js
@@ -14,12 +14,22 @@ const port = 8000;
 
 require('./config/passport')(passport);
 
+// In-memory storage for user data
+let userData = {
+    downloads: [],
+    routine: {}
+};
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Update the CORS configuration
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: [
+        'http://localhost:5173',
+        'chrome-extension://eghidbonaaignaaicojchkkjbfbplljh'
+    ],
     credentials: true
 }));
 
@@ -43,6 +53,30 @@ app.use('/auth', authRoutes);
 app.use('/api/gmail', gmailRoutes);
 app.use('/api/privacy', privacyRoutes);
 app.use('/api/user', userRoutes);
+
+// Downloads routes
+app.post('/downloads', (req, res) => {
+    const downloadData = req.body;
+    console.log('Received download data:', downloadData);
+    userData.downloads.push(downloadData);
+    res.json({ status: 'success', message: 'Download data received' });
+});
+
+app.get('/downloads', (req, res) => {
+    res.json(userData.downloads);
+});
+
+// Routine routes
+app.post('/routine', (req, res) => {
+    const routineData = req.body;
+    console.log(`Received ${routineData.routine} data:`, routineData.data);
+    userData.routine[routineData.routine] = routineData.data;
+    res.json({ status: 'success', message: `${routineData.routine} data received` });
+});
+
+app.get('/routine', (req, res) => {
+    res.json(userData.routine);
+});
 
 app.listen(port, () => {
     console.log(`Server running on http://127.0.0.1:${port}`);
